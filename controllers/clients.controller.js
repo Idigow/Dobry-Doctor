398538e1a513737cffb690b24d04bd4130aa287b
Baisangur <idigov-98@mail.ru>
2021-09-20 21:39:20 +0300

    client

---
+++ controllers/clients.controller.js
@@ -0,0 +1,28 @@
+const Client = require("../models/Client.model");
+
+module.exports.clientController = {
+  getAllClients: async (req, res) => {
+    const users = await Client.find();
+    res.json(users);
+  },
+  getClientById: async (req, res) => {
+    const user = await Client.findById(req.user.id);
+    await res.json(user);
+  },
+  createClient: async (req, res) => {
+    try {
+      const { firstName, lastName, fatherName, phoneNumber } = req.body;
+      const client = await Client.create({
+        firstName,
+        lastName,
+        fatherName,
+        phoneNumber,
+      })
+      await res.json(client);
+    } catch (e) {
+      return res.status(400).json({
+        error: "Ошибка при создании клиента " + e.toString(),
+      });
+    }
+  },
+};
--- routes/index.route.js
+++ routes/index.route.js
@@ -2,5 +2,6 @@ const {Router} = require("express")
 const router = Router()
 
 router.use(require("./users.route"))
+router.use(require("./clients.route"))
 
 module.exports = router
