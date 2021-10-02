6ea1b3d4b644c87799244f59bc35208f781f2db4
Mahammad <idmag95@gmail.com>
2021-10-02 15:30:02 +0300

    внес небольшие изменения

--- clients1/src/components/Header/index.jsx
+++ clients1/src/components/Header/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
-import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import {
+  AppBar, Button, ButtonGroup, Dialog,
+  makeStyles, Toolbar, Typography
+
+} from '@material-ui/core'
+
+import AddClients from '../Clients/AddClients'
+const useStyles = makeStyles({
+  root: {
+    "& button": {
+      color: "white",
+      border: "none"
+    }
+  }
+})
 
 const Header = () => {
+  const [open, setOpen] = useState(false)
+  const handleClickOpen = () =>{
+    setOpen(true)
+  }
+  const handleClose = () =>{
+    setOpen(false)
+  }
+const classes = useStyles()
   return (
     <div>
       <AppBar position="static">
@@ -9,6 +31,18 @@ const Header = () => {
           <Typography variant="h5">
             Добрый Доктор
           </Typography>
+          <ButtonGroup className={classes.root}>
+            <Button onClick={handleClickOpen}>Добавить клиента</Button>
+            <Dialog
+              open={open}
+              onClose={handleClose}
+            aria-labelledby="form-dialog"
+            >
+              <AddClients/>
+            </Dialog>
+            <Button>Two</Button>
+            <Button>Three</Button>
+          </ButtonGroup>
         </Toolbar>
       </AppBar>
     </div>
